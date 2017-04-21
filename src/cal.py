419237d163ad2cfec9a54e5477ba039e2ed35a84
kubocker <kubocker@gmail.com>
2017-04-21 20:38:47 +0900

    コメント削除

--- src/cal.py
+++ src/cal.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import calendar
-import datetime
 
-#print(calendar.month(2017,4))
-#print(calendar.monthcalendar(2017,4))
 
 def calendar_list(year, month):
     for m in month:
         c = calendar.month(int(year), int(m))
         print(c)
-
