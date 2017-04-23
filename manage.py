8f6aa104d3699f8360ae9f2353a486bca991d666
kubocker <kubocker@gmail.com>
2017-04-23 17:36:12 +0900

    make command fix

--- manage.py
+++ manage.py
@@ -1,16 +1,18 @@
 import click
 import subprocess
 
+
 @click.command()
 @click.option('-n', '--name')
 def startapp(name):
     click.echo("make vim app {0}".format(name))
-    cmd = "touch src/{0}.py {0}.vim".format(name)
+    cmd = "touch src/{0}.py plugin/{0}.vim autoload/{0}.vim doc/{0}.txt".format(name)
     subprocess.call(cmd, shell=True)
+
 
 def main():
     startapp()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
